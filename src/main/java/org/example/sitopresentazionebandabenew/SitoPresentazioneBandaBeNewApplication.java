be45b2246b0c7825080997deee7f9dd0f8e2c616
Aurelio Marotta <aurelio@Mac.lan>
2026-01-01 14:10:14 +0100

    first commit

---
+++ src/main/java/org/example/sitopresentazionebandabenew/SitoPresentazioneBandaBeNewApplication.java
@@ -0,0 +1,13 @@
+package org.example.sitopresentazionebandabenew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitoPresentazioneBandaBeNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitoPresentazioneBandaBeNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/sitopresentazionebandabenew/SitoPresentazioneBandaBeNewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sitopresentazionebandabenew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitoPresentazioneBandaBeNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
