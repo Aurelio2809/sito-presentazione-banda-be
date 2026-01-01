2f079037e84124ff8e5c4c8a8a09948c2526c599
aurelio <aurelius99w@hotmail.it>
2026-01-01 14:11:43 +0100

    first commit

--- src/test/java/org/example/sitopresentazionebandabenew/SitoPresentazioneBandaBeNewApplicationTests.java
+++ src/test/java/org/example/sitopresentazionebandabenew/SitoPresentazioneBandaBeNewApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SitoPresentazioneBandaBeNewApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
